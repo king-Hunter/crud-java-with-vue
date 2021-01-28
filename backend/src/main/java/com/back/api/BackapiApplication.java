c65f03337479788d6b609eb07666fc4e8979e6f6
Juan A <esjhuan1000@gmail.com>
2021-01-28 17:10:50 -0600

    backend and frontend skeleton

---
+++ backend/src/main/java/com/back/api/BackapiApplication.java
@@ -0,0 +1,13 @@
+package com.back.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackapiApplication.class, args);
+	}
+
+}
